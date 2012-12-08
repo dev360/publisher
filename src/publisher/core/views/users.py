47830755a9858e0cdf99ec02178ab70f9ef33d78
Igor Guerrero <igfgt1@gmail.com>
2012-12-08 16:15:46 -0500

    Add placeholder

--- src/publisher/core/views/users.py
+++ src/publisher/core/views/users.py
@@ -19,8 +19,10 @@ def user_detail(request, username):
     User detail view
     """
     user = get_object_or_404(User, username=username)
+    feeds = [1, 2, 3]
     return render_to_response('core/users/detail.html', {
         'profile': user.profile,
+        'feeds': feeds,
         'page': 'feeds',
     }, RequestContext(request))
 

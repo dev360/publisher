67513ca93adb7f92f07c87146eaa123c9910784d
Buzzi <kingbuzzman@gmail.com>
2012-12-09 00:08:38 -0500

    Adds registration and payment to the unsubscribed feeds

--- src/publisher/core/views/feeds.py
+++ src/publisher/core/views/feeds.py
@@ -10,10 +10,13 @@ from django.shortcuts import render_to_response, get_object_or_404
 from django.views.decorators.cache import never_cache
 from django.utils.translation import ugettext_lazy, ugettext as _
 from django.views.decorators.cache import cache_page
+from django.views.generic import View
 from django.core.paginator import Paginator, InvalidPage, EmptyPage
 from django.template import RequestContext
+from django.contrib.auth import authenticate
 
-from core.forms import CreateFeedForm
+from auth.forms import RegistrationForm
+from core.forms import CreateFeedForm, PaymentForm
 from core.models import Feed, FeedItem
 
 def feed_detail(request, username, feed_slug):
@@ -32,20 +35,61 @@ def feed_detail(request, username, feed_slug):
         'page': 'feeds',
     }, RequestContext(request))
 
-def feed_detail_subscribe(request, username, feed_slug):
-    """
-    Users feed detail
-    """
-    user = get_object_or_404(User, username=username)
-    feed = get_object_or_404(Feed, publisher=user, slug=feed_slug)
-    feed_items = FeedItem.objects.filter(feed=feed, is_sample=True)[:3]
+class FeedDetailSubscribe(View):
+    def dispatch(self, request, *args, **kwargs):
+        if request.method.lower() in self.http_method_names:
+            handler = getattr(self, request.method.lower(), self.http_method_not_allowed)
+        else:
+            handler = self.http_method_not_allowed
 
-    return render_to_response('core/feeds/detail_subscribe.html', {
-        'profile': user.profile,
-        'feed': feed,
-        'feed_items': feed_items,
-        'page': 'feeds',
-    }, RequestContext(request))
+        self.request = request
+        self.args = args
+        self.kwargs = kwargs
+
+        user = get_object_or_404(User, username=kwargs.get('username'))
+        feed = get_object_or_404(Feed, publisher=user, slug=kwargs.get('feed_slug'))
+
+        return handler(request, user, feed)
+
+    def get(self, request, user, feed):
+        return self.render(user, feed)
+
+    def post(self, request, user, feed):
+        register_form = None
+        if self.request.user.is_anonymous():
+            register_form = RegistrationForm(request.POST)
+        subscribe_form = PaymentForm(request.POST)
+
+        if ((register_form and register_form.is_valid()) or register_form == None) and subscribe_form.is_valid():
+            if register_form:
+                register_user = register_form.save(request)
+                register_user.profile.status = 'REG'
+                register_user.profile.save()
+
+                registed_user = authenticate(
+                    username=request.POST.get('username'),
+                    password=request.POST.get('password')
+                )
+
+            register_form.save(request)
+
+        return self.render(user, feed, register_form, subscribe_form)
+
+    def render(self, user, feed, register_form=None, subscribe_form=PaymentForm()):
+        if not register_form and self.request.user.is_anonymous():
+            register_form = RegistrationForm()
+
+        feed_items = FeedItem.objects.filter(feed=feed, is_sample=True)[:3]
+
+        return render_to_response('core/feeds/detail_subscribe.html', {
+            'profile': user.profile,
+            'feed': feed,
+            'feed_items': feed_items,
+            'page': 'feeds',
+            'register_form': register_form,
+            'subscribe_form': subscribe_form
+        }, RequestContext(self.request))
+
 
 def feed_item_detail(request, username, feed_slug, item_slug):
     """
